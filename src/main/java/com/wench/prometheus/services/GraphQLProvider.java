cd342627aaac8b56839e12f10024ac85b7e7aba3
Wenchy Dutreuil <wcdutreuil@gmail.com>
2021-04-11 14:01:07 -0500

    try 2

--- src/main/java/com/wench/prometheus/services/GraphQLProvider.java
+++ src/main/java/com/wench/prometheus/services/GraphQLProvider.java
@@ -31,7 +31,7 @@ public class GraphQLProvider {
 
 
     // Not working
-    @Value("classpath:graphql/schema.graphqls")
+    @Value("classpath:static/graphql/schema.graphqls")
     private Resource resource;
 
     @Bean
