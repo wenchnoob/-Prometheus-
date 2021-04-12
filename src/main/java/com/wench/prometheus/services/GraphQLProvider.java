72af1f1c57f3e38584e0d2e304edfcb01c482699
Wenchy Dutreuil <wcdutreuil@gmail.com>
2021-04-11 20:24:09 -0500

    sending in a covert operative to inspect

--- src/main/java/com/wench/prometheus/services/GraphQLProvider.java
+++ src/main/java/com/wench/prometheus/services/GraphQLProvider.java
@@ -1,5 +1,6 @@
 package com.wench.prometheus.services;
 
+import com.wench.prometheus.Prometheus;
 import com.wench.prometheus.data.fetchers.CalculationsByUsernameDataFetcher;
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
@@ -7,16 +8,20 @@ import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
+import org.checkerframework.checker.units.qual.C;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -65,10 +70,13 @@ public class GraphQLProvider {
     }
 
     @Autowired
-    private ApplicationContext applicationContext;
+    private ResourceLoader resourceLoader;
 
     private void walk_file_system() throws IOException {
-        File start = applicationContext.getResource("classpath*:.").getFile();
+       File in = resourceLoader.getResource("classpath*:static/graphql/schema.graphqls").getFile();
+       System.out.println(in);
+       //while(in.available() > 0) System.out.println((char)in.read());
+        File start = resourceLoader.getResource("classpath:.").getFile();
         print_dir(start);
     }
 
